13253ee1bda60c16b04003cbfeb04a3a965f67df
whalelab12 <jobugorira211@gmail.com>
2026-01-19 14:38:06 +0900

    コード生成テスト完了（修正が複数箇所ある）

--- comp/tests/prog4.cs
+++ comp/tests/prog4.cs
@@ -1,6 +1,18 @@
-int print();
-int j;
-int i;
-i = 4;
-j = i = i + 5;
-print();
+int i = 0;
+int j = 1;
+if (i == j)
+{
+    i = i + 1;
+}
+else if (i != j)
+{
+    i = i - 1;
+}
+else if (i != j)
+{
+    i = i - 1;
+}
+else if (i != j)
+{
+    i = i - 1;
+}
